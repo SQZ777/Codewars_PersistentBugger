fd5cde8f152610f9a99d6a63df02a95df5ceaf25
sqz777 <sqz777isme@gmail.com>
2018-01-06 15:30:30 +0800

    加入專案檔案。

---
+++ Codewars_PersistentBugger/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_PersistentBugger
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
