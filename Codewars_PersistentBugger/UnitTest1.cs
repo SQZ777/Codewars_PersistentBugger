81c1bb96e09408b0b22a1b76a57e9803c8a896fd
sqz777 <sqz777isme@gmail.com>
2018-01-06 16:07:03 +0800

    pass mutiply and fail test case : input 11

--- Codewars_PersistentBugger/UnitTest1.cs
+++ Codewars_PersistentBugger/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Codewars_PersistentBugger
@@ -10,13 +11,60 @@ namespace Codewars_PersistentBugger
         {
             Assert.AreEqual(0, Persist.Persistence(0));
         }
+
+        [TestMethod]
+        public void Mutiply_Input_0_Should_Be_0()
+        {
+            Assert.AreEqual(0, Persist.DigitMutiply(0));
+        }
+
+        [TestMethod]
+        public void DigitMutiply_Input_1_Should_Be_1()
+        {
+            Assert.AreEqual(1, Persist.DigitMutiply(1));
+        }
+
+        [TestMethod]
+        public void DigitMutiply_Input_11_Should_Be_1()
+        {
+            Assert.AreEqual(1, Persist.DigitMutiply(11));
+        }
+
+        [TestMethod]
+        public void DigitMutiply_Input_23_Should_Be_6()
+        {
+            Assert.AreEqual(6, Persist.DigitMutiply(23));
+        }
+
+        [TestMethod]
+        public void DigitMutiply_Input_123_Should_Be_6()
+        {
+            Assert.AreEqual(6, Persist.DigitMutiply(123));
+        }
+
+        [TestMethod]
+        public void Input_11_Should_Be_1()
+        {
+            Assert.AreEqual(1, Persist.Persistence(11));
+        }
+
     }
 
     public class Persist
     {
-        public static int Persistence(int n)
+        public static int Persistence(long n)
         {
             return 0;
+        }
+
+        public static int DigitMutiply(long n)
+        {
+            var result = Convert.ToInt32(n.ToString()[0].ToString());
+            for (int i = 1; i < n.ToString().Length; i++)
+            {
+                result *= Convert.ToInt32(n.ToString()[i].ToString());
+            }
+            return result;
         }
     }
 }
